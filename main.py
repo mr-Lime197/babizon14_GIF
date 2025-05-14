add69b7cfdb8a18d809c87d2b4b845e552088fa5
Mr.Lime <max-bibikov@mail.ru>
2025-05-14 13:47:49 +0500

    HOST

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from transformers import AutoTokenizer, AutoModel
 from pydantic import BaseModel
 import torch
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
+from fastapi.templating import Jinja2Templates
 
 # Загрузка модели и токенизатора
 tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny2")
@@ -33,12 +34,16 @@ class Item(BaseModel):
     text: str
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 @app.get("/")
-async def gay():
-    return {"message": "Hello, GAY!"}
-@app.get("/gif")
-async def gif(text:str):
-    return sim(text)
+async def read_form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+@app.post("/submit")
+async def handle_form(request: Request, text_input: str = Form(...)):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "result": (text_input, sim(text_input))
+    })
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="192.168.89.169", port=8080)
