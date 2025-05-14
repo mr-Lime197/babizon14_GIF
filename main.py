19f6f67e83f88663b3a83036dc2aa96d95f3dfa6
Mr.Lime <max-bibikov@mail.ru>
2025-05-14 16:22:05 +0500

    refresh

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from pydantic import BaseModel
 import torch
 import json
 from fastapi import FastAPI, Form, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 # Загрузка модели и токенизатора
 tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny2")
 model = AutoModel.from_pretrained("cointegrated/rubert-tiny2")
+with open("static/babiz/link.json", 'r', encoding="UTF-8") as s:
+    video=json.load(s)
+
 
 def get_emb(text):
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
@@ -17,12 +21,20 @@ def get_emb(text):
         outputs = model(**inputs)
         embeddings = outputs.last_hidden_state.mean(dim=1).squeeze().numpy()
     return list(embeddings)
+
+
+
 def dist(point_1,point_2):
     diff = np.array(point_1) - np.array(point_2)
     return(np.dot(diff, diff))
+
+
+emb_seq=dict()
+for seq in video.keys():
+    emb=get_emb(seq)
+    emb_seq[seq]=emb
+
 def sim(text):
-    with open("data.json", "r", encoding="UTF-8") as s:
-        emb_seq=json.load(s)[0]
     emb=get_emb(text)
     best=list(emb_seq.keys())[0]
     for seq in emb_seq.keys():
@@ -33,17 +45,22 @@ def sim(text):
 class Item(BaseModel):
     text: str
 
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 @app.get("/")
 async def read_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 @app.post("/submit")
 async def handle_form(request: Request, text_input: str = Form(...)):
+    global video
+    s=sim(text_input)
     return templates.TemplateResponse("index.html", {
         "request": request,
-        "result": (text_input, sim(text_input))
+        "result": (text_input, s),
+        "video": video[s]
     })
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="192.168.89.169", port=80)
+    uvicorn.run(app, host="127.0.0.1", port=8080)        
