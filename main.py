d731359cb9bbd4dbb2e1e351d1ce166451745df7
Mr.Lime <max-bibikov@mail.ru>
2025-05-19 13:07:17 +0500

    add: NewClasses

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ from transformers import AutoTokenizer, AutoModel,  BertTokenizer, BertModel
 from pydantic import BaseModel
 import torch
 import json
-from fastapi import FastAPI, Form, Request
+import logging
+from fastapi import FastAPI, Form, Request, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy import  create_engine, Column, Integer, String, LargeBinary, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Session, relationship
+database="sqlite:///data.db"
+
+class Base(DeclarativeBase): pass
+class Gif_added(BaseModel):
+    text: str
+    video: UploadFile
+class Gif_info_db(Base):
+    __tablename__="Gif_db"
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    file = Column(LargeBinary)
+    emb=relationship("info", back_populates="Gif_db")
+class Gif_emb_db(Base):
+    __tablename__="Gif_emb_db"
+    id = Column(Integer, primary_key=True, index=True)
+    id_gif=Column(Integer, ForeignKey("Gif_info_db.id"))
+    description=Column(String)
+    info=relationship("emb", back_populates="Gif_emb_db")
+
 
 # Загрузка модели и токенизатора
 tokenizer = AutoTokenizer.from_pretrained("squeezebert/squeezebert-uncased")
@@ -44,9 +66,25 @@ def sim(text):
         if(dist(emb, emb_seq[best])> dist(emb, emb_seq[seq])):
             best=seq
     return best
+class db:
+    def __init__(self, path):
+        db.__path=path
+        db.__engine=create_engine(path, echo=True)
+        Base.metadata.create_all(bind=db.__engine)
+    def add_description(txt_gif: str, txt_desc: str):
+        gif_emb=Gif_emb_db(desription=txt_desc)
+        gif_emb.info=txt_gif
+    def add_gif(self, Gif:Gif_added):
+        gif=Gif_info_db(text=Gif.text, video=Gif.video.file.read())
+        with Session(autoflush=False, bind=db.__engine) as s:
+            if s.query(Gif_info_db).filter(Gif_info_db.file==gif.file).first():
+                logging.error(f'GIFs: {gif.text} is exist')
+                return
+            s.add(gif)
 
-class Item(BaseModel):
-    text: str
+        
+            
+                
 
 
 app = FastAPI()
@@ -64,6 +102,9 @@ async def handle_form(request: Request, text_input: str = Form(...)):
         "result": (text_input, s),
         "video": video[s]
     })
+@app.post("/add_gif")
+async def add_gif(Gif: Gif_added):
+    pass
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="10.89.0.240", port=8080)        
+    uvicorn.run(app, host="127.0.0.1", port=80)
